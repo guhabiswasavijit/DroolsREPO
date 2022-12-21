da5fa58194242ebca10740df995be947b2093bbf
Avijit GuhaBiswas <guhabiswas.avijit@gmail.com>
2022-12-21 12:08:52 +0530

    drool demo

---
+++ src/main/java/org/seven/telecom/DecisionController.java
@@ -0,0 +1,28 @@
+package org.seven.telecom;
+
+import org.seven.telecom.domain.OrderRequest;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DecisionController {
+
+
+    private final KieContainer kieContainer;
+
+    public DecisionController(KieContainer kieContainer) {
+        this.kieContainer = kieContainer;
+    }
+
+    @PostMapping("/discount")
+    private OrderRequest getDiscountPercent(@RequestBody OrderRequest orderRequest) {
+        KieSession kieSession = kieContainer.newKieSession();
+        kieSession.insert(orderRequest);
+        kieSession.fireAllRules();
+        kieSession.dispose();
+        return orderRequest;
+    }
+}
